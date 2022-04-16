8d5a4e330ec3e7d8c85f9187b9a050f3c0f4800c
laxman83 <laxmankavale83@gmail.com>
2022-04-16 17:04:54 +0530

    add frontend code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'clients';
+export class AppComponent implements OnInit {
+  title = 'The Dating Apps';
+  users: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5000/api/user').subscribe(
+      (response) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log('error', error);
+      }
+    );
+  }
 }
